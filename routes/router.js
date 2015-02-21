20fe7dac28a20b7c1005cefc302abe0e68ad1e7d
CodeMySky <codemysky@hotmail.com>
2015-02-21 12:09:09 -0500

    + get friend

--- routes/router.js
+++ routes/router.js
@@ -1,25 +1,78 @@
-var oauth2lib = require('oauth20-provider');
 var query = require("querystring");
-oauth2 = new oauth2lib({log:{level:2}});
-
-function isAuthorized (req, res, next) {
-	if (req.session.authorized) next();
-	else {
-		var params = req.query;
-		params.backUrl = req.path;
-		res.redirect('/login?' + query.stringify(params));
-	}
-}
-
+var settings = require("../settings");
+var json2csv = require('json2csv');
+var fs = require('fs');
+var async = require('async');
+var Linkedin = require('node-linkedin')(settings.api_key, settings.secret, settings.redirect_uri);
+var linkedin;
+var fields = ['id', 'first-name', 'last-name', 'maiden-name',
+            'formatted-name', 'headline', 'location',
+            'industry', 'current-share', 'num-connections', 'num-connections-capped',
+            'summary', 'specialties', 'positions', 'picture-url','picture-urls::(original)',
+            'email-address', 'last-modified-timestamp', 'associations', 'interests',
+            'publications', 'patents', 'languages', 'skills', 'certifications',
+            'educations', 'courses', 'volunteer', 'num-recommenders',
+            'recommendations-received', 'mfeed-rss-url', 'following', 'job-bookmarks',
+            'suggestions', 'date-of-birth', 'related-profile-views', 'honors-awards',
+            'phone-numbers', 'bound-account-types', 'im-accounts', 'main-address',
+            'twitter-accounts', 'primary-twitter-account', 'connections', 'group-memberships',
+            'network', 'public-profile-url','api-standard-profile-request'];
 var router = {
-    setup :function(server){
-        server.use(oauth2.inject());
-        server.post('/token', oauth2.controller.token);
-        server.get('/authorization', isAuthorized,
-        	oauth2.controller.authorization, function (req, res) {
-        	res.render('authorization', {layout: false});
-        });
-        server.post('/authorization', isAuthorized, oauth2.controller.authorization);
+    setup :function(app){
+    	app.get('/oauth/linkedin', function(req, res) {
+    	    // This will ask for permisssions etc and redirect to callback url. 
+    	    // var params = {
+    	    // 	response_type: 'code',
+    	    // 	client_id: settings.api_key,
+    	    // 	redirect_uri: settings.redirect_uri,
+    	    // 	state: 'cool'
+    	    // }
+    	    // res.redirect("https://www.linkedin.com/uas/oauth2/authorization?" + query.stringify(params));
+    	    Linkedin.auth.authorize(res, ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups', 'w_messages']);
+    	});
+    	app.get('/oauth/linkedin/callback', function(req, res) {
+    	    Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
+    	        if ( err )
+    	            return console.err(err);
+    	        results = JSON.parse(results);
+    	        linkedin = Linkedin.init(results.access_token);
+    	        
+    	        return res.redirect('/search');
+    	    });
+    	});
+    	app.get('/search', function (req, res) {
+    		if (linkedin == undefined)
+    			return res.redirect('/oauth/linkedin');
+    		linkedin.people.me(function (err, data) {
+    			if (err) {
+    				res.send({err:err});
+    			} else {
+    				var connections = [];
+    				var count = 0;
+    				async.map(data.connections.values, function (connection, cb) {
+    					if (count > 2) return cb(null, {});
+    					count++;
+    					linkedin.people.id(connection.id,fields,function (err, data) {
+    						var positions = [];
+    						if (data.positions && data.positions._total > 0) {
+    							data.positions = data.positions.values[0].title;
+    						}
+    						cb(err, data);
+    					});
+    				}, function (err, connections) {
+    					json2csv({data: connections, fields: ['id','headline','positions','skills','specialties','interests','educations']}, function(err, csv) {
+						  if (err) console.log(err);
+						  res.send({csv:csv});
+						  fs.writeFile('result.csv', csv, function(err) {
+						    if (err) throw err;
+						    console.log('file saved');
+						  });
+						});
+    				});
+    				
+    			}
+    		})
+    	});
     },
 
 };
