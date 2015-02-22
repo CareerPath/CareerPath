4983ac93c494c20b2cd48f9318ae58300a43f388
CodeMySky <codemysky@hotmail.com>
2015-02-22 15:50:08 -0500

    change toggle message

--- public/js/career-match.js
+++ public/js/career-match.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
     $(".fold-up").click(function(){
     	// $(this).fadeToggle();
         $(this).siblings(".job-detail").fadeToggle();
-        
+        if ($(this).html().indexOf('Fold')>-1) {
+        	$(this).text("Expand");
+        } else {
+        	$(this).text("Fold up");
+        }
     });
 
 
